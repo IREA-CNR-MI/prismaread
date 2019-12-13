396e7755a66043a98a71196ec84a34dc974962d5
lbusett <lbusett@gmail.com>
2019-12-13 12:42:00 +0100

    Fix to avoid error on old processors if ATCOR_wls is not "Nominal"

--- R/convert_prisma.R
+++ R/convert_prisma.R
@@ -160,66 +160,72 @@ convert_prisma <- function(in_file,
     # if specified, save additional wvl files corresponding to selected columns ----
 
     if (ATCOR_wls != "Nominal" && ATCOR == TRUE) {
-      if (!is.numeric(ATCOR_wls)) {
-        stop("ATCOR_wls should be wither \"Nominal\" or a vector containing the",
-             "column numbers at which wavelengths should be retrieved")
-      }
 
-      vnir_start  <- hdf5r::h5attr(f, "Start_index_EO_VNIR")
-      vnir_stop   <- hdf5r::h5attr(f, "Stop_index_EO_VNIR")
-      vnir_wl_mat <- t(f[["//KDP_AUX/Cw_Vnir_Matrix"]][vnir_start:vnir_stop,])
-      vnir_wl_mat <- vnir_wl_mat[,order_vnir]
-      vnir_wl_mat <- vnir_wl_mat[, which(vnir_wl_mat[1,] != 0)]
-      vnir_fwhm_mat <- t(f[["KDP_AUX/Fwhm_Vnir_Matrix"]][vnir_start:vnir_stop,])
-      vnir_fwhm_mat <- vnir_fwhm_mat[,order_vnir]
-      vnir_fwhm_mat <- vnir_fwhm_mat[, which(vnir_fwhm_mat[1,] != 0)]
+      if (hdf5r::existsGroup(f, "//KDP_AUX/Cw_Vnir_Matrix")) {
 
-      swir_start  <- hdf5r::h5attr(f, "Start_index_EO_SWIR")
-      swir_stop   <- hdf5r::h5attr(f, "Stop_index_EO_SWIR")
-      swir_wl_mat <- t(f[["//KDP_AUX/Cw_Swir_Matrix"]][swir_start:swir_stop,])
-      swir_wl_mat <- swir_wl_mat[,order_swir]
-      swir_wl_mat <- swir_wl_mat[, which(swir_wl_mat[1,] != 0)]
-      swir_fwhm_mat <- t(f[["//KDP_AUX/Fwhm_Swir_Matrix"]][swir_start:swir_stop,])
-      swir_fwhm_mat <- swir_fwhm_mat[,order_swir]
-      swir_fwhm_mat <- swir_fwhm_mat[, which(swir_fwhm_mat[1,] != 0)]
+        if (!is.numeric(ATCOR_wls)) {
+          stop("ATCOR_wls should be wither \"Nominal\" or a vector containing the",
+               "column numbers at which wavelengths should be retrieved")
+        }
 
-      if(join_priority == "VNIR") {
-        swir_wl_mat   <- swir_wl_mat[,which(swir_wl_mat[1,] > max(vnir_wl_mat[1,]))]
-        swir_fwhm_mat <- swir_fwhm_mat[,which(swir_wl_mat[1,] > max(vnir_wl_mat[1,]))]
+        vnir_start  <- hdf5r::h5attr(f, "Start_index_EO_VNIR")
+        vnir_stop   <- hdf5r::h5attr(f, "Stop_index_EO_VNIR")
+        vnir_wl_mat <- t(f[["//KDP_AUX/Cw_Vnir_Matrix"]][vnir_start:vnir_stop,])
+        vnir_wl_mat <- vnir_wl_mat[,order_vnir]
+        vnir_wl_mat <- vnir_wl_mat[, which(vnir_wl_mat[1,] != 0)]
+        vnir_fwhm_mat <- t(f[["KDP_AUX/Fwhm_Vnir_Matrix"]][vnir_start:vnir_stop,])
+        vnir_fwhm_mat <- vnir_fwhm_mat[,order_vnir]
+        vnir_fwhm_mat <- vnir_fwhm_mat[, which(vnir_fwhm_mat[1,] != 0)]
+
+        swir_start  <- hdf5r::h5attr(f, "Start_index_EO_SWIR")
+        swir_stop   <- hdf5r::h5attr(f, "Stop_index_EO_SWIR")
+        swir_wl_mat <- t(f[["//KDP_AUX/Cw_Swir_Matrix"]][swir_start:swir_stop,])
+        swir_wl_mat <- swir_wl_mat[,order_swir]
+        swir_wl_mat <- swir_wl_mat[, which(swir_wl_mat[1,] != 0)]
+        swir_fwhm_mat <- t(f[["//KDP_AUX/Fwhm_Swir_Matrix"]][swir_start:swir_stop,])
+        swir_fwhm_mat <- swir_fwhm_mat[,order_swir]
+        swir_fwhm_mat <- swir_fwhm_mat[, which(swir_fwhm_mat[1,] != 0)]
+
+        if(join_priority == "VNIR") {
+          swir_wl_mat   <- swir_wl_mat[,which(swir_wl_mat[1,] > max(vnir_wl_mat[1,]))]
+          swir_fwhm_mat <- swir_fwhm_mat[,which(swir_wl_mat[1,] > max(vnir_wl_mat[1,]))]
+        } else {
+          vnir_wl_mat   <- vnir_wl_mat[,which(vnir_wl_mat[1,] < min(swir_wl_mat[1,]))]
+          vnir_fwhm_mat <- vnir_fwhm_mat[,which(vnir_wl_mat[1,] < min(swir_wl_mat[1,]))]
+        }
+
+        wl_mat_tot    <- cbind(vnir_wl_mat, swir_wl_mat )
+        fwhm_mat_tot  <- cbind(vnir_fwhm_mat, swir_fwhm_mat )
+
+        for (col in ATCOR_wls) {
+          dir.create(file.path(ATCOR_fold, trimws(col)), showWarnings = FALSE)
+          out_file_wvl <- file.path(ATCOR_fold, trimws(col),
+                                    paste0(tools::file_path_sans_ext(basename(out_file)),
+                                           paste0("_atcor_wvl_", trimws(col), ".wvl")))
+          out <- data.frame(`channel number`            = 1:dim(wl_mat_tot)[2],
+                            `channel center wavelength` = round(wl_mat_tot[col,]/1000, digits = 6),
+                            `bandwidth` = round(fwhm_mat_tot[col,], digits = 6),
+                            stringsAsFactors = FALSE)
+          names(out) <- paste(c("channel number", "channel center wavelength", "bandwidth"), col)
+          utils::write.table(out, file = out_file_wvl, row.names = FALSE, sep = "\t")
+
+          out_file_cal <- file.path(ATCOR_fold, trimws(col),
+                                    paste0(tools::file_path_sans_ext(basename(out_file)),
+                                           "_atcor_cal.cal"))
+          out <- data.frame("wavelength" = round(wl_mat_tot[col,]/1000, digits = 6),
+                            `radiometric offset c0` = 0,
+                            `radiometric offset c1` = 1, stringsAsFactors = FALSE)
+          names(out) <- c("wavelength", "radiometric offset c0 ", "radiometric offset c0")
+          utils::write.table(out, file = out_file_cal, row.names = FALSE, sep = "\t")
+
+          out_file_dat <- file.path(ATCOR_fold, trimws(col),
+                                    paste0(tools::file_path_sans_ext(basename(out_file)),
+                                           "atcor_dat.dat"))
+          file.copy(system.file("extdata/atcor_dat.dat", package = "prismaread"),
+                    out_file_dat)
+        }
       } else {
-        vnir_wl_mat   <- vnir_wl_mat[,which(vnir_wl_mat[1,] < min(swir_wl_mat[1,]))]
-        vnir_fwhm_mat <- vnir_fwhm_mat[,which(vnir_wl_mat[1,] < min(swir_wl_mat[1,]))]
-      }
-
-      wl_mat_tot    <- cbind(vnir_wl_mat, swir_wl_mat )
-      fwhm_mat_tot  <- cbind(vnir_fwhm_mat, swir_fwhm_mat )
-
-      for (col in ATCOR_wls) {
-        dir.create(file.path(ATCOR_fold, trimws(col)), showWarnings = FALSE)
-        out_file_wvl <- file.path(ATCOR_fold, trimws(col),
-                                  paste0(tools::file_path_sans_ext(basename(out_file)),
-                                         paste0("_atcor_wvl_", trimws(col), ".wvl")))
-        out <- data.frame(`channel number`            = 1:dim(wl_mat_tot)[2],
-                          `channel center wavelength` = round(wl_mat_tot[col,]/1000, digits = 6),
-                          `bandwidth` = round(fwhm_mat_tot[col,], digits = 6),
-                          stringsAsFactors = FALSE)
-        names(out) <- paste(c("channel number", "channel center wavelength", "bandwidth"), col)
-        utils::write.table(out, file = out_file_wvl, row.names = FALSE, sep = "\t")
-
-        out_file_cal <- file.path(ATCOR_fold, trimws(col),
-                                  paste0(tools::file_path_sans_ext(basename(out_file)),
-                                         "_atcor_cal.cal"))
-        out <- data.frame("wavelength" = round(wl_mat_tot[col,]/1000, digits = 6),
-                          `radiometric offset c0` = 0,
-                          `radiometric offset c1` = 1, stringsAsFactors = FALSE)
-        names(out) <- c("wavelength", "radiometric offset c0 ", "radiometric offset c0")
-        utils::write.table(out, file = out_file_cal, row.names = FALSE, sep = "\t")
-
-        out_file_dat <- file.path(ATCOR_fold, trimws(col),
-                                  paste0(tools::file_path_sans_ext(basename(out_file)),
-                                         "atcor_dat.dat"))
-        file.copy(system.file("extdata/atcor_dat.dat", package = "prismaread"),
-                  out_file_dat)
+        message("CW matrix dataset not existing - creation of additional ATCOR files ignored")
       }
     }
   }
@@ -480,9 +486,9 @@ convert_prisma <- function(in_file,
       if (proc_lev == "1") {
         pan_cube <- f[[paste0("/HDFEOS/SWATHS/PRS_L1_", gsub("H", "P", source), "/Data Fields/Cube")]][,]
         pan_lat <- t(f[[paste0("/HDFEOS/SWATHS/PRS_L1_", gsub("H", "P", source),
-                              "/Geolocation Fields/Latitude")]][,])
+                               "/Geolocation Fields/Latitude")]][,])
         pan_lon <- t(f[[paste0("/HDFEOS/SWATHS/PRS_L1_", gsub("H", "P", source),
-                              "/Geolocation Fields/Longitude")]][,])
+                               "/Geolocation Fields/Longitude")]][,])
       } else {
         pan_cube <- f[[paste0("//HDFEOS/SWATHS/PRS_L2D_PCO/Data Fields/Cube")]][,]
       }
